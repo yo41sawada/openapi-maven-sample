f9cffba20b837c6ec7b851ea7a5ab1c3d06919f2
yo41sawada <wada@nstock.co.jp>
2022-06-01 05:28:26 +0000

    add: test class

--- backend/src/test/java/jp/yo41sawada/backend/BackendApplicationTests.java
+++ backend/src/test/java/jp/yo41sawada/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package jp.yo41sawada.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest( classes = BackendApplicationTests.class )
 class BackendApplicationTests {
 
 	@Test
